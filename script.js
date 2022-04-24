7877970daec14300bd6e1786a4525593bfe30355
unknown <buraevvasilii@gmail.com>
2022-04-24 15:42:17 +0300

    relise

---
+++ script.js
@@ -0,0 +1,34 @@
+const quoteText = document.querySelector(".quote"),
+quoteBtn = document.querySelector("button"),
+authorName = document.querySelector(".name"),
+githubBtn = document.querySelector(".github"),
+copyBtn = document.querySelector(".copy"),
+twitterBtn = document.querySelector(".twitter"),
+synth = speechSynthesis;
+
+function randomQuote(){
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText = "Loading...";
+    fetch("http://api.quotable.io/random").then(response => response.json()).then(result => {
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+        quoteBtn.classList.remove("loading");
+        quoteBtn.innerText = "New Quote";
+    });
+}
+
+githubBtn.addEventListener("click", ()=>{
+  let githubUrl = `https://github.com/Prince-Gizard/random-quote-machine.git`;
+  window.open(githubUrl, "_blank");
+});
+
+twitterBtn.addEventListener("click", ()=>{
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+    window.open(tweetUrl, "_blank");
+});
+
+copyBtn.addEventListener("click", ()=>{
+    navigator.clipboard.writeText(quoteText.innerText);
+});
+
+quoteBtn.addEventListener("click", randomQuote);
